894b9dd0b6161ff865881f87d5b9e6949c66c3e4
Fahadur20Git <fahadur516@gmail.com>
2025-11-04 00:45:46 +0530

    Update types.java

--- types.java
+++ types.java
@@ -1,1 +1,17 @@
-class demo
+import java.util.Scanner;
+
+class Details{
+    public static void main(String args[]){
+	Scanner scan = new Scanner(System.in);
+	int a = scan.nextInt();
+	int b = scan.nextInt();
+	String name = scan.nextLine();
+	int c = a * b;
+	System.out.println("My name is"+" "+name);
+	System.out.println("multiple"+" "+c);
+    }
+}	
+ 
+	
+
+
